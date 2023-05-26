bdabe1bd5efb2acd3ac8f97f93bbb0326bd24750
ganesh <ganesh1011sadar@gmail.com>
2023-05-26 12:54:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SheplyComponent } from './sheply/sheply.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SheplyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sheply/sheply.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import{FormControl,FormGroup,Validators} from '@angular/forms';
+@Component({
+  selector: 'app-sheply',
+  templateUrl: './sheply.component.html',
+  styleUrls: ['./sheply.component.css']
+})
+export class SheplyComponent {
+ 
+}
+
+
